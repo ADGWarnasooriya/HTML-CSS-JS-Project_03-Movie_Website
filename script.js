bb8bdde638f1df8e06cea10473146bece6fd4697
ADGWarnasooriya <phoenixfire696@gmail.com>
2021-11-28 23:10:04 +0530

    JS Arrow

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const arrows = document.querySelectorAll(".arrow");
+const movieLists = document.querySelectorAll(".movie-list");
 
+arrows.forEach((arrow, i) => {
+    arrow.addEventListener("click", () => {
+        console.log("you clicked to: " + i)
+    })
+})
